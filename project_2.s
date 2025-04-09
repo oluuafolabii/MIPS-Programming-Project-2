93f4e00fbbda49f4d38085874e0255c1e56928dd
oluuafolabii <oluuafolabi@gmail.com>
2025-04-08 20:24:33 -0400

    Clear the Input Buffer

--- project_2.s
+++ project_2.s
@@ -12,3 +12,13 @@ null_str:       .asciiz "NULL"
 .globl main
 main:
 
+    la   $t0, input_buffer      # pointer to input_buffer
+    li   $t1, 1001              # number of bytes to clear
+zero_loop:
+    beq  $t1, $zero, zero_done
+    sb   $zero, 0($t0)
+    addi $t0, $t0, 1
+    addi $t1, $t1, -1
+    j    zero_loop
+zero_done:
+
